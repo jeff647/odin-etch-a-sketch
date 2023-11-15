3df79a8b59fbf3ed9bdc2dcfd6126fcec1444edc
Jeff <jeff647@users.noreply.github.com>
2023-11-14 22:02:23 -0500

    Implement darkening and lightening feature

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const gridContainerRule = [...stylesheet.cssRules].find(
   (r) => r.selectorText === ".grid-container"
 );
 
-// Initialize starting 16 x 16 grid.
+// Initialize starting 16 x 16 grid with black mouseover as default event
 createGrid(gridSize);
 
-// Grid size functions
-function createGrid(size) {
+// Create a grid based on size and event parameter
+function createGrid(size, event = makeGridItemBgBlack) {
   for (let i = 0; i < size; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add("grid-item");
+    // Initialize brightness for darkening function
+    gridDiv.style.filter = "brightness(100%)";
     gridContainer.appendChild(gridDiv);
-    gridDiv.addEventListener("mouseover", makeGridItemBgColorBlack);
   }
+  addMouseOverEvent(event);
 }
 
 function customSize() {
@@ -41,6 +43,7 @@ function customSize() {
     gridContainer.removeChild(child);
     child = gridContainer.lastElementChild;
   }
+  // Update css property to resize squares to fit container fully
   gridContainerRule.style.setProperty(
     "grid-template-columns",
     `repeat(${parsedUserSizeInput}, auto)`
@@ -48,28 +51,18 @@ function customSize() {
   createGrid(convertedUserSize);
 }
 
-// Grid style functions
-function makeGridItemBgColorBlack(e) {
-  removeMouseOverEvent(makeGridItemBgColorRandom);
-  addMouseOverEvent(makeGridItemBgColorBlack);
-  e.target.style.backgroundColor = "black";
+// Add event listener to grid items
+function addMouseOverEvent(currentEvent) {
+  removeMouseOverEvent(makeGridItemBgBlack);
+  removeMouseOverEvent(makeGridItemBgRandom);
+  removeMouseOverEvent(makeGridItemBgDarken);
+  removeMouseOverEvent(makeGridItemBgLighten);
+  const nodeList = gridContainer.childNodes;
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].addEventListener("mouseover", currentEvent);
+  }
 }
-
-function makeGridItemBgColorRandom(e) {
-  removeMouseOverEvent(makeGridItemBgColorBlack);
-  addMouseOverEvent(makeGridItemBgColorRandom);
-  e.target.style.backgroundColor = `rgb(${randomIntegerGenerator(
-    256
-  )},${randomIntegerGenerator(256)},${randomIntegerGenerator(256)})`;
-}
-
-function makeGridItemBgColorDarken(e){
-  removeMouseOverEvent(makeGridItemBgColorBlack);
-  removeMouseOverEvent(makeGridItemBgColorRandom);
-  addMouseOverEvent(makeGridItemBgColorDarken);
-  e.target.style.filter = "brightness(0%)";
-}
-
+// Remove event listeners to grid items
 function removeMouseOverEvent(currentEvent) {
   const nodeList = gridContainer.childNodes;
   for (let i = 0; i < nodeList.length; i++) {
@@ -77,14 +70,45 @@ function removeMouseOverEvent(currentEvent) {
   }
 }
 
-function addMouseOverEvent(currentEvent) {
-  const nodeList = gridContainer.childNodes;
-  for (let i = 0; i < nodeList.length; i++) {
-    nodeList[i].addEventListener("mouseover", currentEvent);
-  }
+// Event target style functions
+
+function makeGridItemBgBlack(evt) {
+  evt.target.style.backgroundColor = "hsl(0, 0%, 0%)";
 }
 
-// Helper functions
+function makeGridItemBgRandom(evt) {
+  evt.target.style.backgroundColor = `rgb(${randomIntegerGenerator(
+    256
+  )},${randomIntegerGenerator(256)},${randomIntegerGenerator(256)})`;
+}
+
+// Every time event is invoked make brightness
+
+function makeGridItemBgDarken(evt) {
+  let brightnessLevelArray = evt.target.style.filter.match(numberRegex);
+  let brightnessLevel = parseInt(brightnessLevelArray[0]);
+
+  if (brightnessLevel > 0) {
+    evt.target.style.filter = `brightness(${brightnessLevel - 10}%)`;
+  }
+  console.log(brightnessLevel);
+}
+
+function makeGridItemBgLighten(evt) {
+  let brightnessLevelArray = evt.target.style.filter.match(numberRegex);
+  let brightnessLevel = parseInt(brightnessLevelArray[0]);
+
+  if (brightnessLevel < 100) {
+    evt.target.style.filter = `brightness(${brightnessLevel + 10}%)`;
+  }
+  console.log(brightnessLevel);
+}
+
+// HELPER FUNCTIONS
+
+// Return number from 0 up to but excluding rangeValue
 function randomIntegerGenerator(rangeValue) {
   return Math.floor(Math.random() * rangeValue);
 }
+
+const numberRegex = /\d+/;
